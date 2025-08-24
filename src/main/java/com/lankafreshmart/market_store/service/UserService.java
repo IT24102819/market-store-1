ff95a6e2432015c4c18310218016b7967aa991be
IT24102819 <176378655+IT24102819@users.noreply.github.com>
2025-08-24 21:22:39 +0530

    Update UserService.java
    add update and delete methods

--- src/main/java/com/lankafreshmart/market_store/service/UserService.java
+++ src/main/java/com/lankafreshmart/market_store/service/UserService.java
@@ -20,4 +20,31 @@ public class UserService {
         user.setRole("USER");
         userRepository.save(user);
     }
+
+    public void updateProfile(String currentUsername,User updatedUser){
+        User  user = userRepository.findByUsername(currentUsername)
+                .orElseThrow(() -> new IllegalArgumentException("Username not found"));
+
+        // Update fields only if provided
+        if (updatedUser.getUsername() != null && !updatedUser.getUsername().isEmpty()) {
+            if (!updatedUser.getUsername().equals(currentUsername) &&
+                    userRepository.findByUsername(updatedUser.getUsername()).isPresent()) {
+                throw new IllegalArgumentException("Username already taken");
+            }
+            user.setUsername(updatedUser.getUsername());
+        }
+        if (updatedUser.getEmail() != null && !updatedUser.getEmail().isEmpty()) {
+            user.setEmail(updatedUser.getEmail());
+        }
+        if (updatedUser.getPassword() != null && !updatedUser.getPassword().isEmpty()) {
+            user.setPassword(passwordEncoder.encode(updatedUser.getPassword()));
+        }
+        userRepository.save(user);
+    }
+
+    public void deleteAccount(String username){
+        User  user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        userRepository.delete(user);
+    }
 }
