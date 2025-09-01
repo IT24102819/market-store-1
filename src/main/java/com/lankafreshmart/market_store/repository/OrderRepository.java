3c7a8ddb57d3ee2290d3d1db654159a2b4205bc6
IT24102819 <176378655+IT24102819@users.noreply.github.com>
2025-09-01 21:15:06 +0530

    Update OrderRepository.java

--- src/main/java/com/lankafreshmart/market_store/repository/OrderRepository.java
+++ src/main/java/com/lankafreshmart/market_store/repository/OrderRepository.java
@@ -1,7 +1,11 @@
 package com.lankafreshmart.market_store.repository;
 
 import com.lankafreshmart.market_store.model.Order;
+import com.lankafreshmart.market_store.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUser(User user);
 }
