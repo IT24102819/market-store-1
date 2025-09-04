f5373459d31dbcd5c01790b581225d79d432d6bb
IT24102819 <176378655+IT24102819@users.noreply.github.com>
2025-09-05 00:35:25 +0530

    Update ReviewRepository.java

--- src/main/java/com/lankafreshmart/market_store/repository/ReviewRepository.java
+++ src/main/java/com/lankafreshmart/market_store/repository/ReviewRepository.java
@@ -1,7 +1,11 @@
 package com.lankafreshmart.market_store.repository;
 
+import com.lankafreshmart.market_store.model.Product;
 import com.lankafreshmart.market_store.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByProduct(Product product);
 }
