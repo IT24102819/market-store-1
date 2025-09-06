0b067d9143679a8cc5cf25a8d7138e0864139595
IT24102819 <176378655+IT24102819@users.noreply.github.com>
2025-09-06 18:02:36 +0530

    Update ProductRepository.java

--- src/main/java/com/lankafreshmart/market_store/repository/ProductRepository.java
+++ src/main/java/com/lankafreshmart/market_store/repository/ProductRepository.java
@@ -1,12 +1,16 @@
 package com.lankafreshmart.market_store.repository;
 
-
-
 import com.lankafreshmart.market_store.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByStockQuantityLessThanEqual(int stockQuantity);
+    List<Product> findByNameContainingIgnoreCase(String name);
+    List<Product> findByCategoryIgnoreCase(String category);
+    List<Product> findByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
+    List<Product> findByNameContainingIgnoreCaseAndPriceBetweenAndCategoryIgnoreCase(
+            String name, BigDecimal minPrice, BigDecimal maxPrice, String category);
 }
